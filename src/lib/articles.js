93b0f4712634252cd10b40fb3f6be8fe792560e9
Fenanda <viajerevista@gmail.com>
2026-02-19 15:01:31 -0300

    content: remove Mistral and Rodig essay

--- src/lib/articles.js
+++ src/lib/articles.js
@@ -1,14 +1,7 @@
 export const articles = [
   { 
-    id: 7, 
-    title: '<i>Ella ya casi ha tocado mi frente</i>: fabulaciones del roce entre Rodig y Mistral', 
-    author: 'Fernanda Moreno y Florencia Silva', 
-    href: "/ensayos/ella-ya-casi-ha-tocado-mi-frente", 
-    img: "/MISTRAL.jpg" 
-  },
-  { 
     id: 1, 
-    title: 'Sangre, tierra y silencio: el doble vínculo de la experiencia femenina', 
+    title: 'Cuando la sangre toca la tierra: el doble vínculo de la mujer', 
     author: 'Florencia Leiva Silva', 
     href: "/sangre-tierra-y-silencio", 
     img: "/Carrieee.jpg" 
