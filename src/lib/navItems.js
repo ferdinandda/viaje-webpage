cfe684992d066fb9ecc1fe3eee8549a2c8ef021a
Fenanda <viajerevista@gmail.com>
2026-02-19 13:39:14 -0300

    style: rename Notas to [M]al paso and move to end of nav

--- src/lib/navItems.js
+++ src/lib/navItems.js
@@ -1,7 +1,7 @@
 export const navItems = [
   { href: '/', label: 'Inicio' },
   { href: '/ensayos', label: 'Ensayos' },
-  { href: '/notas', label: 'NOTAS' },
   { href: '/equipo', label: 'Sobre la revista' },
-  { href: '/convocatoria', label: 'Convocatoria' }
+  { href: '/convocatoria', label: 'Convocatoria' },
+  { href: '/notas', label: '[M]al paso' }
 ];
