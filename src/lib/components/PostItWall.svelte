e891500e350f9c52dedf5a3f8974fb2a04931532
Fenanda <viajerevista@gmail.com>
2026-02-18 22:09:51 -0300

    style: revert silhouettes opacity to 0.06

--- src/lib/components/PostItWall.svelte
+++ src/lib/components/PostItWall.svelte
@@ -43,7 +43,7 @@
   
   <!-- Background Image: siluetas.jpg (Tiled for crowd effect) -->
   <div 
-    class="absolute inset-0 z-0 bg-repeat transition-transform duration-[60s] ease-linear hover:scale-105 pointer-events-none grayscale opacity-[0.18]"
+    class="absolute inset-0 z-0 bg-repeat transition-transform duration-[60s] ease-linear hover:scale-105 pointer-events-none grayscale opacity-[0.06]"
     style="background-image: url('/siluetas.jpg'); background-size: 400px auto;"
   ></div>
   
